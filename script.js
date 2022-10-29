436ed7d8ad76afeb055e34301473d59d2f6445f6
Anderson Neves <andersonneves_@hotmail.com>
2022-10-29 12:58:22 -0300

    função assíncrona com async await usando com try catch

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 // aqui define uma função assíncrona e o operador "await" é usado para esperar uma "promise"
 async function buscaEndereco() {
-    var consultaCep = await fetch('https://viacep.com.br/ws/01001000/json/')
-    // convertendo o retorno do fetch em json
-    var consultaCepCovertida = await consultaCep.json();
-    
-    console.log(consultaCepCovertida);
+    try {        
+        var consultaCep = await fetch('https://viacep.com.br/ws/01001250/json/')
+        // convertendo o retorno do fetch em json
+        var consultaCepCovertida = await consultaCep.json();
+        if (consultaCepCovertida.erro) {
+            throw Error('CEP não existente!');
+        }
+        console.log(consultaCepCovertida);
+    } catch (erro) {
+        console.log(erro);
+    }
 }
 
 // chamando buscaEndereco
