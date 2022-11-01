6bd4b7ff7120dcc77b268e48170fc38968749eee
Anderson Neves <andersonneves_@hotmail.com>
2022-11-01 07:56:20 -0300

    pegando o elemento cep da tela e adicionando um evento de focusout que quando o usuário clica em outro local chama a função buscaEndereco retornando no console os valores do cep

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-// aqui define uma função assíncrona e o operador "await" é usado para esperar uma "promise"
-async function buscaEndereco() {
+async function buscaEndereco(cep) {
     try {        
-        var consultaCep = await fetch('https://viacep.com.br/ws/01001250/json/')
-        // convertendo o retorno do fetch em json
+        var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
         var consultaCepCovertida = await consultaCep.json();
         if (consultaCepCovertida.erro) {
             throw Error('CEP não existente!');
         }
         console.log(consultaCepCovertida);
+        return consultaCepCovertida;
     } catch (erro) {
         console.log(erro);
     }
 }
 
-// chamando buscaEndereco
-buscaEndereco();
+var cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
+
+// linha 1, aqui define uma função assíncrona e o operador "await" é usado para esperar uma "promise".
+// linha 4, convertendo o retorno do fetch em json.
+// linha 9, retornando a variável convertida para quem chama essa função esse valor.
+// linha 15 e 16, crio uma variável que guarda o elemento cep digitado na tela pegando pelo "id" dele, e coloco um ouvinte de eventos nele de focus que quando a pessoa digitar fora desse campo ativa ele "focusout" ou focus fora, aí quando ele acontece chama a função buscaEndereco e manda o valor do cep.
